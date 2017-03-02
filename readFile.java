16f2ae19a0ea8dc53f973bb0f93f92a601b5cdc1
Shlok Dixit <shlok.official@gmail.com>
2017-03-02 12:06:13 -0500

    test with new file

---
+++ readFile.java
@@ -0,0 +1,14 @@
+class readFile{
+
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of integers");
+        int i = sc.nextInt;
+
+
+
+
+    }
+}
